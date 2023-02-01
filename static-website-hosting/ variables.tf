2ecf7656910b977cb9e89d89e3272e7723384543
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-02-01 22:01:18 +0100

    commit

---
+++ static-website-hosting/ variables.tf
@@ -0,0 +1,4 @@
+variable "bucket_name" {
+  type    = string
+  default = "chaincute.delete.this.bucket"
+}
--- static-website-hosting/main.tf
+++ static-website-hosting/main.tf
@@ -11,23 +11,42 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket_website_configuration" "example" {
-  bucket = aws_s3_bucket.example.bucket
+resource "aws_s3_bucket" "www_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_policy" "www_bucket" {
+  bucket = aws_s3_bucket.www_bucket.bucket
+
+  policy = jsonencode(
+    {
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Sid       = "PublicReadAccess",
+          Effect    = "Allow",
+          Principal = "*",
+          Action    = "s3:GetObject",
+          Resource  = "arn:aws:s3:::${aws_s3_bucket.www_bucket.bucket}/*",
+          Condition = {
+            StringEquals = {
+              "aws:UserAgent" : "Amazon CloudFront"
+            }
+          }
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_s3_bucket_website_configuration" "www_bucket" {
+  bucket = aws_s3_bucket.www_bucket.bucket
 
   index_document {
     suffix = "index.html"
   }
 
   error_document {
-    key = "error.html"
-  }
-
-  routing_rule {
-    condition {
-      key_prefix_equals = "docs/"
-    }
-    redirect {
-      replace_key_prefix_with = "documents/"
-    }
+    key = "404.html"
   }
 }
